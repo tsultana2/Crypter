2d5400eac693cbd187e6b53398b7237b6ae66cdc
tsultana2 <tsultana451@gmail.com>
2019-01-12 21:02:52 -0500

    added multiple encryption and decryption algos, added clear form and copy form feature, revamped ui

--- custom.js
+++ custom.js
@@ -1,10 +1,142 @@
-function encryptMessage() {
-    var inputBox = document.getElementById("inputbox");
-    var outputBox = document.getElementById("outputbox");
-    outputBox.value = CryptoJS.AES.encrypt(inputBox.text, "assan").toString();
-}
-$(document).ready(function() {
-    $( "#encryptbtn" ).click(function() {
-        alert( "Handler for .click() called." );
+var customSalt = "peaches";
+$(document).ready(function () {
+    var options = ["DES", "AES", "RC4Drop", "TripleDES", "Rabbit"];
+    for (var i = 0; i < options.length; i++) {
+        $("#selectAlgo").append(new Option(options[i], options[i]));
+    }
+    $("#clearbtn").click(function () {
+        $('textarea#inputbox').val("");
+    });
+    $("#copyoutput").click(function () {
+        var copyText =  $("textarea#outputbox");
+        copyText.select();
+        document.execCommand("copy");
+    });
+    $("#copyinput").click(function () {
+        var copyText =  $("textarea#inputbox");
+        copyText.select();
+        document.execCommand("copy");
+    });
+    $("#encryptbtn").click(function () {
+        var algoName = $('#selectAlgo').find(":selected").text();
+        if ($.inArray(algoName, options) !== -1) {
+            var inputBox = $('textarea#inputbox');
+            var outputBox = $('textarea#outputbox');
+            if (algoName === "DES") {
+                outputBox.val(
+                    CryptoJS.DES.encrypt(
+                        inputBox.val(),
+                        customSalt,
+                        {
+                            mode: CryptoJS.mode.CBC,
+                            padding: CryptoJS.pad.Pkcs7
+                        }).toString()
+                );
+            } else if (algoName === "AES") {
+                outputBox.val(
+                    CryptoJS.AES.encrypt(
+                        inputBox.val(),
+                        customSalt,
+                        {
+                            mode: CryptoJS.mode.CBC,
+                            padding: CryptoJS.pad.Pkcs7
+                        }
+                    ).toString()
+                );
+            } else if (algoName === "RC4Drop") {
+                outputBox.val(
+                    CryptoJS.RC4Drop.encrypt(
+                        inputBox.val(),
+                        customSalt,
+                        {
+                            mode: CryptoJS.mode.CBC,
+                            padding: CryptoJS.pad.Pkcs7
+                        }
+                    ).toString()
+                );
+            } else if (algoName === "TripleDES") {
+                outputBox.val(
+                    CryptoJS.TripleDES.encrypt(
+                        inputBox.val(),
+                        customSalt,
+                        {
+                            mode: CryptoJS.mode.CBC,
+                            padding: CryptoJS.pad.Pkcs7
+                        }
+                    ).toString()
+                );
+            } else if (algoName === "Rabbit") {
+                outputBox.val(
+                    CryptoJS.Rabbit.encrypt(
+                        inputBox.val(),
+                        customSalt,
+                        {
+                            mode: CryptoJS.mode.CBC,
+                            padding: CryptoJS.pad.Pkcs7
+                        }
+                    ).toString()
+                );
+            }
+        }
+
+    });
+    $("#decryptbtn").click(function () {
+        var algoName = $('#selectAlgo').find(":selected").text();
+        if ($.inArray(algoName, options) !== -1) {
+            var inputBox = $('textarea#inputbox');
+            var outputBox = $('textarea#outputbox');
+            if (algoName === "DES") {
+                outputBox.val(
+                    CryptoJS.DES.decrypt(
+                        inputBox.val(),
+                        customSalt,
+                        {
+                            mode: CryptoJS.mode.CBC,
+                            padding: CryptoJS.pad.Pkcs7
+                        }).toString(CryptoJS.enc.Utf8)
+                );
+            } else if (algoName === "AES") {
+                outputBox.val(
+                    CryptoJS.AES.decrypt(
+                        inputBox.val(),
+                        customSalt,
+                        {
+                            mode: CryptoJS.mode.CBC,
+                            padding: CryptoJS.pad.Pkcs7
+                        }).toString(CryptoJS.enc.Utf8)
+                );
+            } else if (algoName === "RC4Drop") {
+                outputBox.val(
+                    CryptoJS.RC4Drop.decrypt(
+                        inputBox.val(),
+                        customSalt,
+                        {
+                            mode: CryptoJS.mode.CBC,
+                            padding: CryptoJS.pad.Pkcs7
+                        }).toString(CryptoJS.enc.Utf8)
+                );
+            } else if (algoName === "TripleDES") {
+                outputBox.val(
+                    CryptoJS.TripleDES.decrypt(
+                        inputBox.val(),
+                        customSalt,
+                        {
+                            mode: CryptoJS.mode.CBC,
+                            padding: CryptoJS.pad.Pkcs7
+                        }).toString(CryptoJS.enc.Utf8)
+                );
+            } else if (algoName === "Rabbit") {
+                outputBox.val(
+                    CryptoJS.Rabbit.decrypt(
+                        inputBox.val(),
+                        customSalt,
+                        {
+                            mode: CryptoJS.mode.CBC,
+                            padding: CryptoJS.pad.Pkcs7
+                        }).toString(CryptoJS.enc.Utf8)
+                );
+            }
+        }
+
     });
 });
